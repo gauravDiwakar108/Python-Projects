0584c6f56013cdfcb89695bc74903c5e337a42a2
Gaurav <gauravsubhash9999@gmail.com>
2025-11-29 16:12:50 +0530

    bug fixed

--- beginner/guess_the_number/guess_the_number.py
+++ beginner/guess_the_number/guess_the_number.py
@@ -2,18 +2,18 @@
 
 import random
 
-def getMinValue():
+def get_min_value():
     return int(input("Enter min value : ").strip())
 
-def getMaxValue():
+def get_max_value():
     return int(input("Enter max value : ").strip())
 
-def randomNum(x, y):
+def random_num(x, y):
     # set guess number
     rand_num = random.randint(x, y)
     return rand_num
 
-def getUserGuess(x, y):
+def get_user_guess(x, y):
     # getting user guess
     return int(input(f"Guess a number b/w {x} and {y} : ").strip())
 
@@ -25,17 +25,26 @@ def guess(x, y):
         return "Too High. Guess again!"
     return "\nCorrect Guess!!!"
 
+
+def get_valid_input(prompt_func):
+    while True:
+        try:
+            return prompt_func()
+        except ValueError as ve:
+            print(f"Invalid input : {ve}\n")
+
 def main():
-    min_val = getMinValue()
-    max_val = getMaxValue()
-    random_num = randomNum(min_val, max_val)
+    min_val = get_valid_input(get_min_value)
+    max_val = get_valid_input(get_max_value)
+    
+    rand_num = random_num(min_val, max_val)
     user_guess = 0
     guess_count = 0
     
-    while user_guess != random_num:
+    while user_guess != rand_num:
         print()
-        user_guess = getUserGuess(min_val, max_val)
-        result = guess(random_num, user_guess)
+        user_guess = get_valid_input(lambda : get_user_guess(min_val, max_val))
+        result = guess(rand_num, user_guess)
         guess_count += 1
         print(result)
     print(f"User guessed the nubmer in {guess_count} guess.")
